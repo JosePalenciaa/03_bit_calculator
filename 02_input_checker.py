7888a9ee29b2383fdb271a2495118ff9a0171a77
JosePalenciaa <100240798+JosePalenciaa@users.noreply.github.com>
2022-04-04 21:08:16 +1200

    Add files via upload

--- 02_input_checker.py
+++ 02_input_checker.py
@@ -5,14 +5,20 @@ from email.mime import image
 
 def user_choice():
 
-    valid = false
+    valid = False
     while not valid:
 
-        response = "File type (integer / text / image): ". lower(), text(), image()
+        response = input("File type (integer / text / image): "). lower()
 
         if response == "text" or response == "t":
-            return response
+            return "text"
 
         else:
             print("PLease choose a valid file type!")
             print()
+
+# Main routine goes here
+data_type = user_choice()
+print("You successfully chosen", data_type)
+
+print()
