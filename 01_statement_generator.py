72258790c67b7af9d5b2c3301497fee81f61dd03
JosePalenciaa <100240798+JosePalenciaa@users.noreply.github.com>
2022-04-04 20:46:08 +1200

    Add files via upload

--- 01_statement_generator.py
+++ 01_statement_generator.py
@@ -3,4 +3,17 @@
 # Puts series of symbols at start and end of text (for emphasis)
 def statement_generator(text, decoration):
 
-    #Makes strin
+    #Makes string with five characters
+    ends = decoration * 5
+
+    # add decoration to start and end of statement
+    statement = "{} {} {}".format(ends, text, ends)
+
+    print()
+    print(statement)
+    print()
+
+    return ""
+
+# Main routine goes here
+statement_generator("Hello World", "-")
