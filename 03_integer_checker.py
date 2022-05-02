bf259342cebe9826f134e749ddb2248dd4e9df7e
JosePalenciaa <100240798+JosePalenciaa@users.noreply.github.com>
2022-05-02 12:39:14 +1200

    Add files via upload

---
+++ 03_integer_checker.py
@@ -0,0 +1,23 @@
+def num_check(question):
+    valid = False
+    while not valid:
+        
+        error = "Please enter a number that is more than zero."
+        
+        try:
+        
+            # ask user to enter a number
+            response = float(input(question))
+            
+            # checks number is more than zero
+            if response > 0:
+                return response
+            
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+                
+        except ValueError:
+            print(error)    
+    
