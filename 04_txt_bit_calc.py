c2d829aa358f90d622ab703f010315131b762d5b
JosePalenciaa <100240798+JosePalenciaa@users.noreply.github.com>
2022-05-16 12:53:18 +1200

    Add files via upload

--- 04_txt_bit_calc.py
+++ 04_txt_bit_calc.py
@@ -4,3 +4,7 @@ def text_bits():
     print()
     # Ask user for a string...
     var_text = input("Enter some text: ")
+
+    # Calculates the # of bits (length of sting x 8)
+    var_length = len(   var_text)
+    num_bits = 8 * var_length
