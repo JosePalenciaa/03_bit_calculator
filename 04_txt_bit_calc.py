c8020d879ce742bc9766e96457b35eed76662e88
JosePalenciaa <100240798+JosePalenciaa@users.noreply.github.com>
2022-05-16 12:57:55 +1200

    Add files via upload

--- 04_txt_bit_calc.py
+++ 04_txt_bit_calc.py
@@ -8,3 +8,16 @@ def text_bits():
     # Calculates the # of bits (length of sting x 8)
     var_length = len(   var_text)
     num_bits = 8 * var_length
+
+    # Outputs answer with workingtt
+    print()
+    print("\'{}\' has {} characters ...". format(var_text, var_length))
+    print("# of bits is {} x 8...".format(var_length))
+    print("We need {} bits to represent {}".format(num_bits, var_text))
+    print()
+
+    return ""
+
+
+# Main routine goes here
+text_bits()
